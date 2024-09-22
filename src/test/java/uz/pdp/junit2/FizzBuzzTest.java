0a9a3734a65b9c8886e14b87b7d29bc9d88e29eb
AbbosbekNorinboyev <norinboyevabboskhan002@gmail.com>
2024-09-22 21:19:43 +0500

    TEST DRIVEN DEVELOPMENT

---
+++ src/test/java/uz/pdp/junit2/FizzBuzzTest.java
@@ -0,0 +1,50 @@
+package uz.pdp.junit2;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FizzBuzzTest {
+    FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    void testForNotNullValues() {
+        assertNotNull(fizzBuzz.convert(1));
+    }
+
+    @Test
+    void testForGivenNumber() {
+        assertEquals("1", fizzBuzz.convert(1));
+        assertEquals("2", fizzBuzz.convert(2));
+    }
+
+    @Test
+    void testForFizz() {
+        assertEquals("Fizz", fizzBuzz.convert(3));
+    }
+
+    @Test
+    void testForBuzz() {
+        assertEquals("Buzz", fizzBuzz.convert(5));
+        assertEquals("Buzz", fizzBuzz.convert(10));
+    }
+
+//    @Test
+//    void testForFizzBuzz() {
+//        assertEquals("FizzBuzz", fizzBuzz.convert(15));
+//    }
+
+    @ParameterizedTest(name = "{displayName} => {index} : {arguments}")
+    @CsvFileSource(resources = "/source.csv", useHeadersInDisplayName = true)
+    void testForFizzBuzz(int a, String expected) {
+        assertEquals(expected, fizzBuzz.convert(a));
+    }
+}
